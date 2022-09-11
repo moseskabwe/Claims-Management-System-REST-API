7c099c5a5854f10c98ac1acc1b35638268035e34
MosesKabwe <moseskabwe@gmail.com>
2022-09-11 20:55:01 +0200

    Add search method

--- src/main/java/com/simplehomeinsurance/claims_management_system/controller/PolicyHolderRestController.java
+++ src/main/java/com/simplehomeinsurance/claims_management_system/controller/PolicyHolderRestController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.simplehomeinsurance.claims_management_system.entity.PolicyHolder;
@@ -20,6 +22,11 @@ public class PolicyHolderRestController {
 	@RequestMapping("/policyholders")
 	public List<PolicyHolder> getPolicyholders() {	
 		return policyHolderService.getPolicyHolderList();	
+	}
+	
+	@PostMapping("/policyholders")
+	public List<PolicyHolder> searchPolicyholders(@RequestParam String term) {
+		return policyHolderService.searchPolicyHolders(term);
 	}
 	
 	@GetMapping("/policyholders/{policyHolderNumber}")
